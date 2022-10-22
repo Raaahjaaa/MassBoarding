b2369b0e52898fd752757421946e2a05f227655c
mshawn383 <mshawn383@gmail.com>
2022-10-23 03:52:41 +0530

    added json reader and jsonnode for manipulating the json file

--- src/main/java/com/example/demoWebsite/Controller/DemoController.java
+++ src/main/java/com/example/demoWebsite/Controller/DemoController.java
@@ -2,24 +2,38 @@ package com.example.demoWebsite.Controller;
 
 
 import com.example.demoWebsite.Entity.DemoEntityPojo;
+import com.example.demoWebsite.Entity.StatusReview;
 import com.example.demoWebsite.Service.DemoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 public class DemoController {
 
 
-    @Autowired
+    @Value("classpath:read.json")
+    Resource resource;
+    ArrayList<String> arr=new ArrayList<>();
     private DemoService demoService;
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
 
     @GetMapping("/getData")
     public ResponseEntity getData(){
@@ -27,14 +41,27 @@ public class DemoController {
     }
 
     @GetMapping("/get")
-    public void getdemo(@RequestBody DemoEntityPojo demoEntityPojo) throws JsonProcessingException {
-        ObjectMapper m=new ObjectMapper();
-        String str=m.writerWithDefaultPrettyPrinter()
-                        .writeValueAsString(demoEntityPojo.getMerchantProfile());
-        System.out.println(str);
-if(demoEntityPojo.getMerchantProfile()==null){
-    System.out.println("Got You");
-}
-        System.out.println(demoEntityPojo.getMerchantProfile());
+    public void getdemo(@RequestBody DemoEntityPojo demoEntityPojo) throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+
+        File file=resource.getFile();
+        JsonNode jsonNode=mapper.readTree(file);
+//     System.out.println(jsonNode.fieldNames());
+        Iterator<String> st=jsonNode.get("merchant").fieldNames();
+        while(st.hasNext()){
+            arr.add(st.next());
+        }
+        for(int i=0;i<arr.size();i++){
+            String s= String.valueOf(jsonNode.get("merchant").get(arr.get(i)).get("value"));
+            System.out.println(s);
+        }
+//StatusReview str=mapper.readValue(file, StatusReview.class);
+//        String str=m.writerWithDefaultPrettyPrinter()
+//                        .writeValueAsString(demoEntityPojo.getMerchantProfile());
+//        System.out.println(str);
+//if(demoEntityPojo.getMerchantProfile()==null){
+//    System.out.println("Got You");
+//}
+//        System.out.println(demoEntityPojo.getMerchantProfile());
     }
 }
---
+++ src/main/java/com/example/demoWebsite/Entity/StatusReview.java
@@ -0,0 +1,46 @@
+package com.example.demoWebsite.Entity;
+
+public class StatusReview {
+    private Object merchantProfile;
+    private String locationAddress;
+    private String financial;
+    private String solution;
+    private String bussinessLegalVerification;
+    private String principalInformationSigner;
+
+    public Object getMerchantProfile() {
+        return merchantProfile;
+    }
+
+    public String getLocationAddress() {
+        return locationAddress;
+    }
+
+    public String getFinancial() {
+        return financial;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public String getBussinessLegalVerification() {
+        return bussinessLegalVerification;
+    }
+
+    public String getPrincipalInformationSigner() {
+        return principalInformationSigner;
+    }
+
+    @Override
+    public String toString() {
+        return "StatusReview{" +
+                "merchantProfile=" + merchantProfile +
+                ", locationAddress='" + locationAddress + '\'' +
+                ", financial='" + financial + '\'' +
+                ", solution='" + solution + '\'' +
+                ", bussinessLegalVerification='" + bussinessLegalVerification + '\'' +
+                ", principalInformationSigner='" + principalInformationSigner + '\'' +
+                '}';
+    }
+}
